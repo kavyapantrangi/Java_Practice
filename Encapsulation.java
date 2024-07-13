12e3e36bcef32ec580881473c793e081bdb34c6b
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-07-13 19:31:20 +0530

    Create Encapsulation.java

---
+++ Encapsulation.java
@@ -0,0 +1,7 @@
+ENCAPSULATION:wrapping up the implementation by hiding the data vearibles and methods.
+  focus on the hiding the complexity
+    Examples: school bag which hides the books,pens etc in bag
+             bank account by showing only necessary details required to user
+Abstraction: Hiding the unecessary details and showing the valuable info
+            focus on the data security
+   example: social media apps ,which  we login by using username and password which does not know the what internal process is working
