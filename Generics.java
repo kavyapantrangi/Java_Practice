9c5edcb355d3b781b5e298e3407ed310ffd5e56c
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-07-19 20:09:35 +0530

    Update Generics.java

--- Generics.java
+++ Generics.java
@@ -1,3 +1,6 @@
+<T> is the template for all type of objects
+The Generics are used for the tighter type check at compile time and actual object creation done at runtime,
+cannot create the instance of a type parameter.
 package Generics;
 
 import java.util.Arrays;
@@ -79,7 +82,110 @@ public class CustomArrayList<T> {
         System.out.println(str);
 
 
+    }
+}
+//WIldcard Entries
+? can be known as wildcard in generic programmng
+The wildcard can be used in a variety of  situaction such as type parameter,field,local variable
+UpperBound Wiuldcard Entries
+    when you want to restrict the type in   an arrayList then wildcard entries useful
+    suppose <T exteds Number>means it can only accepts the Number Type and whnever we want use the types of Integer,DOuble  we will use syntax as
+    public void getList(<List<?extends Number>list)
+    package Generics;
 
+import java.util.Arrays;
+import java.util.*;
+
+public class Wildcard<T extends Number> {
+    private Object[] data;
+    private int DEFAULT_SIZE = 10;
+    private int size = 0;
+
+    public Wildcard() {
+        this.data = new Object[DEFAULT_SIZE];
+    }
+
+    public void getList(List<? extends Number> list) {
+    }
+
+    public void add(T num) {
+        if (isFull()) {
+            resize();
+        }
+        data[size++] = num;
+    }
+
+    private boolean isFull() {
+        return size == data.length;
+
+    }
+
+    public void resize() {
+        Object[] temp = new Object[data.length * 2];
+        for (int i = 0; i < data.length; i++) {
+            temp[i] = data[i];
+        }
+        data = temp;
+    }
+
+    public T remove() {
+        T removed = (T) data[--size];
+        return removed;
+
+    }
+
+    public T get(int index) {
+        return (T) data[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void set(int index, int value) {
+        data[index] = value;
+
+    }
+
+    @Override
+    public String toString() {
+        return "wildcard{" +
+                "data=" + Arrays.toString(data) +
+                ", DEFAULT_SIZE=" + DEFAULT_SIZE +
+                ", size=" + size +
+                '}';
+    }
+
+
+    public static void main(String[] args) {
+//        CustomArrayList list=new CustomArrayList();
+//        list.add(6);
+//        list.add(7);
+//        list.add(14);
+//        System.out.println(list);
+//        System.out.println(list.get(0));
+//        System.out.println(list.size);
+//        list.set(0,56);
+//        System.out.println(list.get(0));
+        Wildcard<Integer> str = new Wildcard<>();
+        str.add(1);
+        str.add(2);
+        System.out.println(str);
+        Wildcard<Double>wild=new Wildcard<>();
+        for(int i=0;i<5;i++)
+        {
+            wild.add(5.5*i);
+        }
+        System.out.println(wild);
 
     }
 }
+
+
+
+
+
+
+    
+
+
