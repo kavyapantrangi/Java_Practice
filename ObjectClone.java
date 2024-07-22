82fd9718eda27a27a1ecec91ed58a748ffd22298
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-07-22 16:15:32 +0530

    Create ObjectClone.java

---
+++ ObjectClone.java
@@ -0,0 +1,33 @@
+
+
+human.java
+package Cloning;
+//object cloning is that copying the same elements
+//non primitives will be pointed and primitives will be copied
+public class Human implements Cloneable
+{
+    int age;
+    String name;
+    public Human(int age,String name)
+    {
+        this.age=age;
+        this.name=name;
+    }
+    public Object clone() throws CloneNotSupportedException
+    {
+        return super.clone();
+    }
+}
+
+//main
+package Cloning;
+
+public class Main {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Human h=new Human(21,"kavya");
+        Human k=(Human)h.clone();
+        System.out.println(k.age);
+        System.out.println(k.name);
+
+    }
+}
