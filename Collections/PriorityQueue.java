7611d4042a85b5ca206f5cca26b5358957ad2d7d
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-07-27 22:07:25 +0530

    Create PriorityQueue.java

---
+++ Collections/PriorityQueue.java
@@ -0,0 +1,5 @@
+--> Priority Queue works on priority.
+-->The implementation is based on priority heap,a tree like structure that yields an element at the head 
+  of the queue according to the priority ordering ,which is defined by natural ordering.
+  -->Elemetnts in Priority Queue is not in soring order.
+  
