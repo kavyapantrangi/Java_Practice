8d0cc09aa9eb1c80bb7852fdc9c13adf1032272d
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-07-29 23:29:10 +0530

    Update Comparator.java

--- Collections/Comparator.java
+++ Collections/Comparator.java
@@ -1,7 +1,13 @@
 compare-Comparable interface-compare two objects
   compareTo-Comperator interface- compare one  object
   The Comparable interface has compareTo(T obj) method which is used by sorting methods
-  Comparator provides the two way of sorting
+  The comparator provides the two way of sorting.
+  The comparable interface-natural ordering and The comparator interface has total ordering.
+  The Comperator has more preference than the Comparable
+  
+  
+  class:customcomperator
+  
   package collections;
 
 import java.util.Comparator;
@@ -13,3 +19,37 @@ public class CusomComperator implements Comparator<Integer> {
         return o2-o1;
     }
 }
+//When we want to print the top  elements
+// 0 1 2 100
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Priority1 {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new CusomComperator());
+        pq.offer(1);
+        pq.offer(2);
+        pq.offer(0);
+        pq.offer(100);
+        List<Integer> top2 = new ArrayList<>();
+        int index = 0;
+        while (!pq.isEmpty()) {
+            if (index == 2)
+                break;
+            top2.add(pq.poll());
+            index++;
+        }
+        System.out.println(top2);
+        
+      System.out.println(pq);
+
+
+    }
+
+
+}
+//output is:100,2
+//order
