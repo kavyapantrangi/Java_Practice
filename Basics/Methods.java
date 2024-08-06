9aab2f1667fffebfa259ceabe53e27efeb1d9867
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-08-06 08:44:11 +0530

    Update Methods.java

--- Basics/Methods.java
+++ Basics/Methods.java
@@ -68,6 +68,58 @@ public class Methods {
 
 }
 
+//Shadowing 
+The overriding the value of the existing variable 
+public class Methods {
+   static int a=10;
+    public static void main(String[] args) {
+
+        {
+          int  a=20;
+            System.out.println(a);
+        }
+        System.out.println(a);
+        pro();
+
+
+
+    }
+    static void   pro()
+
+    {
+        int a=50;
+        System.out.println(a);
+    }
+
+
+//Method Overloading
+  package basics;
+
+import java.util.Scanner;
+
+public class Methods {
+
+    public static void main(String[] args) {
+
+        //method overloading
+        System.out.println(   sum(1,2));
+        System.out.println(sum("kavya"));
+        System.out.println(sum(2,3,4));
+
+    }
+    public static int sum(int a,int b)
+    {
+        return a+b;
+    }
+    public static int sum(int a,int b,int c)
+    {
+        return a+b+c;
+    }
+    public static String sum(String s)
+    {
+        return "Hello "+s;
+    }
+
 
 
 
