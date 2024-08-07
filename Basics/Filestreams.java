3c0fb6a0fe60aeeb3ec96e384e069aff25be6224
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-08-07 23:01:02 +0530

    Create Filestreams.java

---
+++ Basics/Filestreams.java
@@ -0,0 +1,10 @@
+
+Streams-->Sequence of data:byte or character.It is also an abstraction that java provides
+  The implementaion is provided by java .io package
+  Types of streams:1.Byte Stream--pdf or files
+                   2.Character Stream -->
+  
+  Byte Stream is divided into types:Input stream,Output Stream
+  Character Stream divided into 2 types:Reader and writer
+  
+  
