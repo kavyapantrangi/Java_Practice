c248e9a6252c870e3cf80a50e3192356ce041a67
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-08-08 22:25:49 +0530

    Update Filestreams.java

--- Basics/Filestreams.java
+++ Basics/Filestreams.java
@@ -6,5 +6,69 @@ Streams-->Sequence of data:byte or character.It is also an abstraction that java
   
   Byte Stream is divided into types:Input stream,Output Stream
   Character Stream divided into 2 types:Reader and writer
+
+
+
+
+  
+  package basics;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FileHandling {
+    //System,out--->std os-console//print stream
+    //System.in--->std in-->keyboard//inputstream
+    //System.err-->std err->console//print stream
+    public static void main(String[] args) {
+        try(InputStreamReader is=new InputStreamReader(System.in))
+        {
+            System.out.print("Enter some letters");
+            int letters=is.read();
+            while(is.ready())
+            {
+                System.out.println((char)letters);
+                letters=is.read();
+            }
+            is.close();
+            System.out.println();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+
+}
+
+//file reader:
+package basics;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.*;
+
+public class FileHandling {
+    //System,out--->std os-console//print stream
+    //System.in--->std in-->keyboard//inputstream
+    //System.err-->std err->console//print stream
+    public static void main(String[] args) {
+        //file reader
+        try(FileReader is=new FileReader("C:\\Users\\bobby\\IdeaProjects\\Cllections\\src\\basics\\ex"))
+        {
+            System.out.print("Enter some letters");
+            int letters=is.read();
+            while(is.ready())
+            {
+                System.out.println((char)letters);
+                letters=is.read();
+            }
+            is.close();
+            System.out.println();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
   
   
