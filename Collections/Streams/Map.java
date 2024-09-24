0e0eccf2efbf02ddb51a4c0f5955748a7e7f73e6
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-09-24 11:38:50 +0530

    Update Map.java

--- Collections/Streams/Map.java
+++ Collections/Streams/Map.java
@@ -17,3 +17,44 @@ public class Map {
         }).forEach(i-> System.out.println(i));
     }
 }
+
+: Intermediate Operation always returns a stream
+    //terminal operations:foreach,collct
+
+
+    //collect
+    package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Map {
+    public static void main(String[] args) {
+
+
+
+        List<Integer> list= Arrays.asList(2,5,7,6,8,9);
+     
+        Stream<Integer>li=list.stream().filter(i->i>4);
+        List<Integer>collect=li.collect(Collectors.toList());
+        System.out.println(collect);
+
+
+    }
+}
+//using count
+public class Map {
+    public static void main(String[] args) {
+
+
+
+        List<Integer> list= Arrays.asList(2,5,7,6,8,9);
+
+        Stream<Integer>li=list.stream().filter(i->i>4);
+
+
+        //using count
+       long count=li.count();
+        System.out.println( "total count is "+count);
