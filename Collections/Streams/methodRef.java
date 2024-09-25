eac8dbc1fe4fa10d04acc54228514a3f61ea1d72
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-09-25 21:11:05 +0530

    Create methodRef.java

---
+++ Collections/Streams/methodRef.java
@@ -0,0 +1,35 @@
+package streams;
+
+import java.sql.SQLOutput;
+
+public class sumTest {
+    public static void main(String[] args) {
+        Isum isum=new Isum() {
+            //ananonymous inner class
+            @Override
+            public int sum(int val1, int val2) {
+                return val1+val2;
+            }
+        };
+
+        System.out.println(" by using anonymous inner class  "+ isum.sum(2,4));
+
+        //by suing java8 features
+        Isum Lambdasum=(val1,val2)->
+        {
+            return val1+val2;
+        };
+        System.out.println("By suing lambda "+Lambdasum.sum(2,6));
+        //by suing method refernece:
+        Isum isum2=sumTest::addition;
+        System.out.println("By suing Method referenec"+isum2.sum(1,2));
+        Isum sum2=Integer::sum;
+        System.out.println("The sum is "+sum2.sum(45,45));
+    }
+    public static int addition(int v1,int v2)
+    {
+        return v1+v2;
+    }
+
+
+}
