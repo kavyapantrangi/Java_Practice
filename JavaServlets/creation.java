ee7b1f1c8cf52168016391c2d0663890a37b7c6f
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-10-20 19:05:23 +0530

    Create creation.java

---
+++ JavaServlets/creation.java
@@ -0,0 +1,27 @@
+Creation of servlet:
+package com.value;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {	
+public void service(HttpServletRequest req,HttpServletResponse res)throws IOException
+
+{
+	int i=Integer.parseInt(req.getParameter("num1"));
+	int j=Integer.parseInt(req.getParameter("num2"));
+	int k=i+j;
+	PrintWriter out=res.getWriter();
+	out.println("result is"+k);
+	
+	}
+}
+// how ro use session:
+HttpSession session=req.getSession();
+//cookie
+Cookie cookie=new Cookie("","")
+  req.addCookie();
